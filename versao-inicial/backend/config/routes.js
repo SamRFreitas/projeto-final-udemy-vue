297691d0266accb2d19f95b2c3f871c89d00e94b
SamRFreitas <samuelrfreitasi@gmail.com>
2020-03-31 17:30:30 -0300

    Api de categories implementada

--- versao-inicial/backend/config/routes.js
+++ versao-inicial/backend/config/routes.js
@@ -5,4 +5,18 @@ module.exports = app => {
 
     app.route('/users/:id')
         .put(app.api.user.save)
+        .get(app.api.user.getById)
+
+    app.route('/categories')
+        .get(app.api.category.get)
+        .post(app.api.category.save)
+
+    // Cuidado com a ordem! Tem que vir antes de /categories/:id
+    app.route('/categories/:id')
+        .get(app.api.category.getTree)
+
+    app.route('/categories/:id')
+        .put(app.api.category.save)
+        .get(app.api.category.getById)
+        .delete(app.api.category.remove)
 }
