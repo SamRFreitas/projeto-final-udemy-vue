9e99e67c2c8c58e6c35ca032c7a944c2f89a3410
SamRFreitas <samuelrfreitasi@gmail.com>
2020-06-08 14:58:12 -0300

    Cadastrode Usuários #2

--- versao-inicial/frontend/src/components/home/Home.vue
+++ versao-inicial/frontend/src/components/home/Home.vue
@@ -1,21 +1,54 @@
 <template>
     <div class="home">
         <PageTitle icon="fa fa-home" main="Dashboard" sub="Base de conhecimento"/>
+        
+        <div class="stats">
+            <Stat title="Categories" :value="stat.categories" 
+                icon="fa fa-folder" color="#D54D50" />
+            
+            <Stat title="Artigos" :value="stat.articles" 
+                icon="fa fa-file" color="#3BC480" />
+            
+            <Stat title="Usuários" :value="stat.users" 
+                icon="fa fa-user" color="#3282CD" />
+        </div>
     </div>
 </template>
 
 <script>
 
 import PageTitle from '../template/PageTitle'
+import Stat from './Stat'
+import axios from 'axios'
+import { baseApiUrl } from '@/global'
 
 export default {
     name: 'Home',
     components: {
-        PageTitle
+        PageTitle,
+        Stat
+    },
+    data() {
+        return {
+            stat: {},
+            reste: 'asas'
+        }
+    },
+    methods: {
+        getStats() {
+            axios.get(`${baseApiUrl}/stats`).then(res => this.stat = res.data)
+        }
+    },
+    mounted() {
+        this.getStats()
     }
 }
 </script>
 
 <style>
-
+    .stats {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
 </style>
--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -3,10 +3,16 @@ import Vue from 'vue'
 
 import App from './App'
 import store from './config/store'
+import './config/msgs'
 import router from './config/router'
+// import axios from 'axios'
 import './config/bootstrap.js'
 
 Vue.config.productionTip = false
+
+// console.log(axios.defaults.headers.common);
+// require('axios').defaults.header.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NiwibmFtZSI6IkFkbWluIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTkxNjI0NzIzLCJleHAiOjE1OTE4ODM5MjN9.GQTh0zPYcZ2LGU5nmbxsQy5gYJwslpZs56vBy-vV5B0'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NiwibmFtZSI6IkFkbWluIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTkxNjI0NzIzLCJleHAiOjE1OTE4ODM5MjN9.GQTh0zPYcZ2LGU5nmbxsQy5gYJwslpZs56vBy-vV5B0'
 
 new Vue({
   store,
